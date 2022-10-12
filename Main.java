b2ada9056d4378cf9145febd47c4d42ff8992fa8
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-10-12 13:04:05 +1100

    main method

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setSpeed(30);
+        car.go();
+        car.stop();
+
+
+
+    }
+}
